f54a556d1c2b9b0ea15da9924f5f2a581551f494
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-07-01 22:03:54 -0400

    907

---
+++ stack/907SumOfSubarrayMinimums.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def sumSubarrayMins(self, A):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        A = [0]+A
+        result = [0]*len(A)
+        stack = [0]
+        for i in range(len(A)):
+            while A[stack[-1]] > A[i]:
+                stack.pop() 
+            j = stack[-1]
+            result[i] = result[j] + (i-j)*A[i]
+            stack.append(i)
+        return sum(result) % (10**9+7)
