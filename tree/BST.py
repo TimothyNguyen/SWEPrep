7f6d5cc0f5ec2d3de1de5a4269d19a6335de0906
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-07-04 17:45:17 -0400

    Add some bst code

--- tree/BST.py
+++ tree/BST.py
@@ -8,10 +8,31 @@ class Tree:
             return None
         
         def insert(root, node):
-            if root.val < node.data:
+            if root.val < node.val:
                 if root.left: insert(root.left, node)
                 else: root.left = node
             else:
                 if root.right: insert(root.right, node)
                 else: root.right = node
         insert(self.root, node)
+    
+    def traverse(self):
+        if self.root is None: return None
+
+        def dfs(node):
+            print("preorder", node.val)
+            if node.left: dfs(node.left)
+            print("inorder", node.val)
+            if node.right: dfs(node.right)
+            print("postorder", node.val)
+        dfs(self.root)
+    
+    def bfs(self):
+        if self.root is None: return None
+        queue = [self.root]
+        while queue:
+            node = queue.pop(0)
+            print(node.val)
+            if node.left: queue.append(node.left)
+            if node.right: queue.append(node.right)
+    
