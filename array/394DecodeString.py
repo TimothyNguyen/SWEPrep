d38277dcd83cee0904e9a4785e3699ce9f5faf46
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-07-20 20:19:15 -0400

    394

---
+++ array/394DecodeString.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def decodeString(self, s):
+        stack = []; curNum = 0; curString = ''
+        for c in s:
+            if c == '[':
+                stack.append(curString)
+                stack.append(curNum)
+                curString = ''
+                curNum = 0
+            elif c == ']':
+                num = stack.pop()
+                prevString = stack.pop()
+                curString = prevString + num*curString
+            elif c.isdigit():     # curNum*10+int(c) is helpful in keep track of more than 1 digit number
+                curNum = curNum*10 + int(c)
+            else:
+                curString += c
+        return curString
