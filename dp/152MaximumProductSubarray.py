f61bc18c7c6979807ece39bdf162257b4be97bca
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-07-31 21:43:20 -0400

    152

--- dp/152MaximumProductSubarray.py
+++ dp/152MaximumProductSubarray.py
@@ -1,15 +1,19 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if not nums or len(nums) == 0: return 0
-        max_arr, min_arr = [0] * len(nums), [0] * len(nums)
-        max_arr[0], min_arr[0] = nums[0], nums[0]
-        result = nums[0]
+        if len(nums) == 0:
+            return 0
+
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+
         for i in range(1, len(nums)):
-            if nums[i] > 0:
-                min_arr[i] = min(nums[i], min_arr[i-1]*nums[i])
-                max_arr[i] = max(nums[i], max_arr[i-1]*nums[i])
-            else:
-                min_arr[i] = min(nums[i], max_arr[i-1]*nums[i])
-                max_arr[i] = max(nums[i], min_arr[i-1]*nums[i])
-            result = max(result, max_arr[i])
+            curr = nums[i]
+            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
+            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
+
+            max_so_far = temp_max
+
+            result = max(max_so_far, result)
+
         return result
