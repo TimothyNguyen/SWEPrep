01e236210a1fd1d767dff46a97e2889a21c35deb
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-08-04 19:22:44 -0400

    237

---
+++ heap/1353MaximumNumberOfEventsThatCanBeAttended.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxEvents(self, A: List[List[int]]) -> int:
+        A.sort(reverse=1)
+        h = []
+        res = d = 0
+        while A or h:
+            if not h: d = A[-1][0]
+            while A and A[-1][0] <= d:
+                heapq.heappush(h, A.pop()[1])
+            heapq.heappop(h)
+            res += 1
+            d += 1
+            while h and h[0] < d:
+                heapq.heappop(h)
+        return res
