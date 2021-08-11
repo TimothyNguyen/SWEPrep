1ada67178af6ee29f2590e64bc7ac195a7ff4aef
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-08-10 23:16:22 -0400

    New stuff

--- design/716MaxStack.py
+++ design/716MaxStack.py
@@ -1,23 +1,43 @@
-class MaxStack(list):
-    def push(self, x):
-        m = max(x, self[-1][1] if self else None)
-        self.append((x, m))
+class MaxStack:
 
-    def pop(self):
-        return list.pop(self)[0]
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.s1 = []
+        self.s2 = []
+        
 
-    def top(self):
-        return self[-1][0]
+    def push(self, x: int) -> None:
+        max_num = x if not self.s2 else self.s2[-1]
+        self.s2.append(max_num if max_num > x else x)
+        self.s1.append(x)
 
-    def peekMax(self):
-        return self[-1][1]
+    def pop(self) -> int:
+        self.s2.pop()
+        return self.s1.pop()
 
-    def popMax(self):
-        m = self[-1][1]
-        b = []
-        while self[-1][0] != m:
-            b.append(self.pop())
+    def top(self) -> int:
+        return self.s1[-1]
 
+    def peekMax(self) -> int:
+        return self.s2[-1]
+
+    def popMax(self) -> int:
+        max_num = self.peekMax()
+        buffer = []
+        while self.top() != max_num:
+            buffer.append(self.pop())
         self.pop()
-        map(self.push, reversed(b))
-        return m
+        while buffer:
+            self.push(buffer.pop())
+        return max_num
+
+
+# Your MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()
