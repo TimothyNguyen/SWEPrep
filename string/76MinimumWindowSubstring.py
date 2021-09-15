353b28b24b997cccdb35202c3f6c00f479f7c1ca
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-09-14 22:59:15 -0400

    New stuff

--- string/76MinimumWindowSubstring.py
+++ string/76MinimumWindowSubstring.py
@@ -1,3 +1,20 @@
+'''
+Given two strings s and t of lengths m and n respectively, return the minimum window 
+substring of s such that every character in t (including duplicates) is included in 
+the window. If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+'''
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         Tmap = Counter(t)
