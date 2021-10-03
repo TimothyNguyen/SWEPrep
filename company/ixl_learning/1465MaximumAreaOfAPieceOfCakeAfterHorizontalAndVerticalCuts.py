dacbc4605e1288e4254ce40cb7b70e69951f9d8c
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-10-03 14:34:45 -0400

    Modify

--- company/ixl_learning/1465MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts.py
+++ company/ixl_learning/1465MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts.py
@@ -1,3 +1,17 @@
+'''
+You are given a rectangular cake of size h x w and two arrays of integers 
+horizontalCuts and verticalCuts where:
+
+horizontalCuts[i] is the distance from the top of the rectangular cake to the 
+ith horizontal cut and similarly, and
+
+verticalCuts[j] is the distance from the left of the rectangular cake to the 
+jth vertical cut.
+
+Return the maximum area of a piece of cake after you cut at each horizontal 
+and vertical position provided in the arrays horizontalCuts and verticalCuts. 
+Since the answer can be a large number, return this modulo 109 + 7.
+'''
 class Solution:
     def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
         # Start by sorting the inputs
