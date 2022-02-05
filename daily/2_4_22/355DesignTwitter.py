bd5b775b83293078fc88cd65b422d3610c537f76
Timothy Nguyen <quynhthoa1972@gmail.com>
2022-02-05 00:57:19 -0500

    Design twitter

---
+++ daily/2_4_22/355DesignTwitter.py
@@ -0,0 +1,91 @@
+'''
+Design a simplified version of Twitter where users can post tweets, follow/unfollow 
+another user, and is able to see the 10 most recent tweets in the user's news feed.
+
+Implement the Twitter class:
+
+Twitter() 
+    - Initializes your twitter object.
+void postTweet(int userId, int tweetId):
+    - Composes a new tweet with ID tweetId by the user userId. Each call to this function 
+    will be made with a unique tweetId.
+List<Integer> getNewsFeed(int userId) 
+    - Retrieves the 10 most recent tweet IDs in the user's news feed. Each item in 
+    the news feed must be posted by users who the user followed or by the user themself. 
+    Tweets must be ordered from most recent to least recent.
+void follow(int followerId, int followeeId) 
+    - The user with ID followerId started following the user with ID followeeId.
+void unfollow(int followerId, int followeeId)
+    - The user with ID followerId started unfollowing the user with ID followeeId.
+ 
+
+Example 1:
+
+Input
+["Twitter", "postTweet", "getNewsFeed", "follow", "postTweet", "getNewsFeed", "unfollow", "getNewsFeed"]
+[[], [1, 5], [1], [1, 2], [2, 6], [1], [1, 2], [1]]
+Output
+[null, null, [5], null, null, [6, 5], null, [5]]
+
+Explanation
+Twitter twitter = new Twitter();
+twitter.postTweet(1, 5); // User 1 posts a new tweet (id = 5).
+twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5]. return [5]
+twitter.follow(1, 2);    // User 1 follows user 2.
+twitter.postTweet(2, 6); // User 2 posts a new tweet (id = 6).
+twitter.getNewsFeed(1);  // User 1's news feed should return a list with 2 tweet ids -> [6, 5]. Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+twitter.unfollow(1, 2);  // User 1 unfollows user 2.
+twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5], since user 1 is no longer following user 2.
+'''
+import time
+from collections import defaultdict
+
+class Tweet:
+    def __init__(self, tweetId, timestamp):
+        self.tweetId = tweetId
+        self.timestamp = timestamp
+class Twitter:
+
+    def __init__(self):
+        self.followers_map = defaultdict(list)
+        self.user_tweets = defaultdict(list)
+        
+        
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        newTweet = Tweet(tweetId, time.time())
+        self.user_tweets[userId].append(newTweet)
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        all_tweets = []
+        all_tweets = all_tweets + self.user_tweets[userId]
+        
+    
+        follow_list = self.followers_map[userId]
+        for user in follow_list:
+            all_tweets = all_tweets + self.user_tweets[user]
+        
+        all_tweets.sort(key = lambda r: r.timestamp, reverse=True)
+        news_feed = all_tweets
+        if len(all_tweets)>10:
+            news_feed = all_tweets[0:10]
+        tweetIds = [x.tweetId for x in news_feed]
+        return tweetIds
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followeeId not in self.followers_map[followerId]:
+            self.followers_map[followerId].append(followeeId)
+        
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.followers_map[followerId]:
+            self.followers_map[followerId].remove(followeeId)
+        
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
