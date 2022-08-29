2fbc220713af732279ce44133036ab76c5a8bc43
Timothy Nguyen <quynhthoa1972@gmail.com>
2022-08-29 18:46:29 -0400

    103

--- leetcode75/l3/hard/25ReverseNodesInK-Group.py
+++ leetcode75/l3/hard/25ReverseNodesInK-Group.py
@@ -21,6 +21,7 @@ k is a positive integer and is less than or equal to the length of the linked li
 You may not alter the values in the list's nodes, only nodes themselves may be changed.
 '''
 # Definition for singly-linked list.
+# O(N) time/O(1) space
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
