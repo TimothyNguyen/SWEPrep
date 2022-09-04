f8d7604efe782f85dea2b4190d1e727cd23d1b5f
Timothy Nguyen <quynhthoa1972@gmail.com>
2022-09-04 17:36:30 -0400

    Fix

--- stack/739DailyTemperatures.py
+++ stack/739DailyTemperatures.py
@@ -2,13 +2,13 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack = [0]
         ans = [0] * len(temperatures)
-        for r in range(1, len(temperatures)):
-            while stack and temperatures[stack[-1]] < temperatures[r]:
-                ans[stack[-1]] = r - stack[-1]
-                stack.pop()
-            stack.append(r)
+        for curr_day, curr_temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < curr_temp:
+                prev_day = stack.pop()
+                ans[prev_day] = curr_day - prev_day
+            stack.append(curr_day)
         return ans
-
+        
 # Example: Input: temperatures = [73,74,75,71,69,72,76,73]
 # Output: [1,1,4,2,1,1,0,0]
 
