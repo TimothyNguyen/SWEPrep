040fb06d563f5bf9f9b0791325152f0f113ea0f0
Timothy Nguyen <quynhthoa1972@gmail.com>
2022-09-07 08:03:34 -0400

    981

---
+++ binary-search/981TimeBaskedKeyStorage.py
@@ -0,0 +1,102 @@
+from sortedcontainers import SortedDict
+
+'''
+Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+
+Implement the TimeMap class:
+
+TimeMap() Initializes the object of the data structure.
+void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+ 
+
+Example 1:
+
+Input
+["TimeMap", "set", "get", "get", "set", "get", "get"]
+[[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
+Output
+[null, null, "bar", "bar", null, "bar2", "bar2"]
+
+Explanation
+TimeMap timeMap = new TimeMap();
+timeMap.set("foo", "bar", 1);  // store the key "foo" and value "bar" along with timestamp = 1.
+timeMap.get("foo", 1);         // return "bar"
+timeMap.get("foo", 3);         // return "bar", since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 is "bar".
+timeMap.set("foo", "bar2", 4); // store the key "foo" and value "bar2" along with timestamp = 4.
+timeMap.get("foo", 4);         // return "bar2"
+timeMap.get("foo", 5);         // return "bar2"
+
+If M is the number of set function calls, N is 
+the number of get function calls, and L 
+is average length of key and value strings.
+Time: O(L * M * log(M)) for get
+      O(N * (L * log M + log M)) for set
+Space: O(M * L) for set
+       O(N) for get
+'''
+class TimeMap:
+
+    def __init__(self):
+        self.time_map = dict()
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        # if key not exists, put in map
+        if key not in self.time_map:
+            self.time_map[key] = SortedDict()
+        
+        self.time_map[key][timestamp] = value
+
+    def get(self, key: str, timestamp: int) -> str:
+        if not key in self.time_map:
+            return ""
+        
+        # Do binary search and look on getting info on right
+        it = self.time_map[key].bisect_right(timestamp)
+        
+        # if iterator points to first element it means, no time <= timestamp exists
+        if it == 0: return ""
+        
+        # Return value stored at previous position of current iterator
+        return self.time_map[key].peekitem(it - 1)[1]
+        
+'''
+class TimeMap:
+    def __init__(self):
+        self.key_time_map = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        # If the 'key' does not exist in dictionary.
+        if not key in self.key_time_map:
+            self.key_time_map[key] = []
+            
+        # Store '(timestamp, value)' pair in 'key' bucket.
+        self.key_time_map[key].append([ timestamp, value ])
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        # If the 'key' does not exist in dictionary we will return empty string.
+        if not key in self.key_time_map:
+            return ""
+        
+        if timestamp < self.key_time_map[key][0][0]:
+            return ""
+        
+        left = 0
+        right = len(self.key_time_map[key])
+        
+        while left < right:
+            mid = (left + right) // 2
+            if self.key_time_map[key][mid][0] <= timestamp:
+                left = mid + 1
+            else:
+                right = mid
+
+        # If iterator points to first element it means, no time <= timestamp exists.
+        return "" if right == 0 else self.key_time_map[key][right - 1][1]
+'''
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
