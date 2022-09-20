239342875297ac7792b20913c610b776def7f667
Timothy Nguyen <quynhthoa1972@gmail.com>
2022-09-19 20:29:25 -0400

    6658

--- leetcode75/l3/medium/658FindKClosestElements.py
+++ leetcode75/l3/medium/658FindKClosestElements.py
@@ -20,3 +20,26 @@ Output: [1,2,3,4]
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         # Base case
+        if len(arr) == k:
+            return arr
+        
+        # Find the closest element and initialize two pointers
+        left = bisect_left(arr, x) - 1
+        right = left + 1
+
+        # While the window size is less than k
+        while right - left - 1 < k:
+            # Be careful to not go out of bounds
+            if left == -1:
+                right += 1
+                continue
+            
+            # Expand the window towards the side with the closer number
+            # Be careful to not go out of bounds with the pointers
+            if right == len(arr) or abs(arr[left] - x) <= abs(arr[right] - x):
+                left -= 1
+            else:
+                right += 1
+        
+        # Return the window
+        return arr[left + 1:right]
