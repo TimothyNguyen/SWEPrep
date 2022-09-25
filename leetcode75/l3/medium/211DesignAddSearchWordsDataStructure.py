86ddb62f76a68bbd62a7220f1e817f94a8fda200
Timothy Nguyen <quynhthoa1972@gmail.com>
2022-09-25 00:25:33 -0400

    211

---
+++ leetcode75/l3/medium/211DesignAddSearchWordsDataStructure.py
@@ -0,0 +1,71 @@
+'''
+Design a data structure that supports adding new words and finding if a string matches any previously added string.
+
+Implement the WordDictionary class:
+
+WordDictionary() Initializes the object.
+void addWord(word) Adds word to the data structure, it can be matched later.
+bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+ 
+
+Example:
+
+Input
+["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+[[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+Output
+[null,null,null,null,false,true,true,true]
+
+Explanation
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.addWord("bad");
+wordDictionary.addWord("dad");
+wordDictionary.addWord("mad");
+wordDictionary.search("pad"); // return False
+wordDictionary.search("bad"); // return True
+wordDictionary.search(".ad"); // return True
+wordDictionary.search("b.."); // return True
+ 
+
+Constraints:
+
+1 <= word.length <= 25
+word in addWord consists of lowercase English letters.
+word in search consist of '.' or lowercase English letters.
+There will be at most 3 dots in word for search queries.
+At most 104 calls will be made to addWord and search.
+
+'''
+class WordDictionary:
+
+    def __init__(self):
+        self.trie = dict()
+
+    def addWord(self, word: str) -> None:
+        node = self.trie
+        for ch in word:
+            if not ch in node:
+                node[ch] = dict()
+            node = node[ch]
+        node['$'] = True
+        
+
+    def search(self, word: str) -> bool:
+        def search_in_node(word, node) -> bool:
+            for i, ch in enumerate(word):
+                if not ch in node:
+                    if ch == '.':
+                        for x in node:
+                            if x != '$' and search_in_node(word[i + 1:], node[x]):
+                                return True
+                    return False
+                node = node[ch]
+            return '$' in node
+        node = self.trie
+        return search_in_node(word, node)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
