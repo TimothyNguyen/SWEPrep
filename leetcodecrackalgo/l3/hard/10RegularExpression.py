f55fe397a780bb916f224c317153e23d51e57a38
Timothy Nguyen <quynhthoa1972@gmail.com>
2022-10-08 14:14:19 -0400

    10

---
+++ leetcodecrackalgo/l3/hard/10RegularExpression.py
@@ -0,0 +1,31 @@
+'''
+Given an input string s and a pattern p, implement regular 
+expression matching with support for '.' and '*' where:
+
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+'''
+class Solution(object):
+    def isMatch(self, s: str, p: str) -> bool:
+        s, p = ' '+ s, ' '+ p
+        lenS, lenP = len(s), len(p)
+        dp = [[0]*(lenP) for i in range(lenS)]
+        dp[0][0] = 1
+
+        for j in range(1, lenP):
+            if p[j] == '*':
+                dp[0][j] = dp[0][j-2]
+
+        for i in range(1, lenS):
+            for j in range(1, lenP):
+                if p[j] in {s[i], '.'}:
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j] == "*":
+                    dp[i][j] = dp[i][j-2] or int(dp[i-1][j] and p[j-1] in {s[i], '.'})
+
+        return bool(dp[-1][-1])
