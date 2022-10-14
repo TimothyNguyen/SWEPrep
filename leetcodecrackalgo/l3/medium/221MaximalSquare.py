4ab42e9b51e4bea92714f517e7d08e55cabf82c6
Timothy Nguyen <quynhthoa1972@gmail.com>
2022-10-13 22:04:14 -0400

    221

--- leetcodecrackalgo/l3/medium/221MaximalSquare.py
+++ leetcodecrackalgo/l3/medium/221MaximalSquare.py
@@ -16,4 +16,24 @@ class Solution:
                     dp[r][c] = min(min(dp[r][c-1], dp[r-1][c]), dp[r-1][c-1]) + 1
                     max_ans = max(max_ans, dp[r][c])
         return max_ans * max_ans
+'''
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        m = len(matrix)
+        n = len(matrix[0]) if m > 0 else 0
+        # dp = [[0 for i in range(n + 1)] for j in range(m + 1)]
+        dp = [0 for i in range(n + 1)]
+        max_ans = 0
+        prev = 0
+        for r in range(1, m + 1):
+            for c in range(1, n + 1):
+                temp = dp[c]
+                if matrix[r-1][c-1] == '1':
+                    dp[c] = min(min(dp[c-1], dp[c]), prev) + 1
+                    max_ans = max(max_ans, dp[c])
+                else:
+                    dp[c] = 0
+                prev = temp
+        return max_ans * max_ans
         
+'''
