62909fb8c03e76cf9016df88ed31aa04e28e6b01
Timothy Nguyen <quynhthoa1972@gmail.com>
2022-10-13 22:48:35 -0400

    410

---
+++ dp/410SplitArrayLargestSum.py
@@ -0,0 +1,65 @@
+'''
+Given an integer array nums and an integer k, split nums into k non-empty 
+subarrays such that the largest sum of any subarray is minimized.
+
+Return the minimized largest sum of the split.
+
+A subarray is a contiguous part of the array.
+
+Example 1:
+
+Input: nums = [7,2,5,10,8], k = 2
+Output: 18
+Explanation: There are four ways to split nums into two subarrays.
+The best way is to split it into [7,2,5] and [10,8], where the 
+largest sum among the two subarrays is only 18.
+
+Example 2:
+
+Input: nums = [1,2,3,4,5], k = 2
+Output: 9
+Explanation: There are four ways to split nums into two subarrays.
+The best way is to split it into [1,2,3] and [4,5], where the largest sum 
+among the two subarrays is only 9.
+'''
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        lo, hi = max(nums), sum(nums)
+        while lo < hi:
+            mid = (lo+hi)//2
+            tot, cnt = 0, 1
+            for num in nums:
+                if tot+num<=mid: 
+                    tot += num
+                else:
+                    tot = num
+                    cnt += 1
+            if cnt>k: lo = mid+1
+            else: hi = mid
+        return hi
+'''
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int n = nums.length;
+        int[][] f = new int[n + 1][m + 1];
+        int[] sub = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                f[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            sub[i + 1] = sub[i] + nums[i];
+        }
+        f[0][0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                for (int k = 0; k < i; k++) {
+                    f[i][j] = Math.min(f[i][j], Math.max(f[k][j - 1], sub[i] - sub[k]));
+                }
+            }
+        }
+        return f[n][m];        
+    }
+}
+'''
