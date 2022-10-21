0b931d793389fcbdf805590f26ef333d80ec4843
Timothy Nguyen <quynhthoa1972@gmail.com>
2022-10-20 23:13:35 -0400

    174

---
+++ leetcodecrackalgo/l3/hard/174DungeonGame.py
@@ -0,0 +1,115 @@
+'''
+The demons had captured the princess and imprisoned her in the bottom-right 
+corner of a dungeon. The dungeon consists of m x n rooms laid out in a 2D grid. 
+Our valiant knight was initially positioned in the top-left room and must fight 
+his way through dungeon to rescue the princess.
+
+The knight has an initial health point represented by a positive integer. If at 
+any point his health point drops to 0 or below, he dies immediately.
+
+Some of the rooms are guarded by demons (represented by negative integers), so 
+the knight loses health upon entering these rooms; other rooms are either empty 
+(represented as 0) or contain magic orbs that increase the knight's health 
+(represented by positive integers).
+
+To reach the princess as quickly as possible, the knight decides to move only 
+rightward or downward in each step.
+
+Return the knight's minimum initial health so that he can rescue the princess.
+
+Note that any room can contain threats or power-ups, even the first room the 
+knight enters and the bottom-right room where the princess is imprisoned.
+
+Example 1:
+
+Input: dungeon = [[-2,-3,3],[-5,-10,1],[10,30,-5]]
+Output: 7
+Explanation: The initial health of the knight must be at least 7 if he 
+follows the optimal path: RIGHT-> RIGHT -> DOWN -> DOWN.
+
+Example 2:
+
+Input: dungeon = [[0]]
+Output: 1
+'''
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        rows, cols = len(dungeon), len(dungeon[0])
+        dp = [[float('inf') for _ in range(cols)] for _ in range(rows)]
+
+        def get_min_health(currCell, nextRow, nextCol):
+            if nextRow >= rows or nextCol >= cols:
+                return float('inf')
+            nextCell = dp[nextRow][nextCol]
+            return max(1, nextCell - currCell)
+        
+        for row in range(rows - 1, -1, -1):
+            for col in range(cols - 1, -1, -1):
+                currCell = dungeon[row][col]
+                right_health = get_min_health(currCell, row, col + 1)
+                down_health = get_min_health(currCell, row+1, col)
+                next_health = min(right_health, down_health)
+
+                if next_health != float('inf'):
+                    min_health = next_health
+                else:
+                    min_health = 1 if currCell >= 0 else (1 - currCell)
+                dp[row][col] = min_health
+        return dp[0][0]
+
+
+class MyCircularQueue:
+    def __init__(self, capacity):
+        """
+        Set the size of the queue to be k.
+        """
+        self.queue = [0]*capacity
+        self.tailIndex = 0
+        self.capacity = capacity
+
+    def enQueue(self, value):
+        """
+        Insert an element into the circular queue.
+        """
+        self.queue[self.tailIndex] = value
+        self.tailIndex = (self.tailIndex + 1) % self.capacity
+
+    def get(self, index):
+        return self.queue[index % self.capacity]
+
+
+class Solution(object):
+    def calculateMinimumHP(self, dungeon):
+        """
+        :type dungeon: List[List[int]]
+        :rtype: int
+        """
+        rows, cols = len(dungeon), len(dungeon[0])
+        # Use a circular queue to keep a sliding window of DP values
+        dp = MyCircularQueue(cols)
+
+        def get_min_health(currCell, nextRow, nextCol):
+            if nextRow < 0 or nextCol < 0:
+                return float('inf')
+            index = cols * nextRow + nextCol
+            nextCell = dp.get(index)
+            # hero needs at least 1 point to survive
+            return max(1, nextCell - currCell)
+
+        for row in range(rows):
+            for col in range(cols):
+                # iterate the grid in the reversed order
+                currCell = dungeon[rows-row-1][cols-col-1]
+
+                right_health = get_min_health(currCell, row, col-1)
+                down_health = get_min_health(currCell, row-1, col)
+                next_health = min(right_health, down_health)
+
+                if next_health != float('inf'):
+                    min_health = next_health
+                else:
+                    min_health = 1 if currCell >= 0 else (1 - currCell)
+
+                dp.enQueue(min_health)
+        # return the last element in the queue
+        return dp.get(cols-1)
