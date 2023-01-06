c6e534967411eb00759f600ba175cbb5cc4b9ef6
Timothy Nguyen <quynhthoa1972@gmail.com>
2023-01-06 17:00:16 -0500

    DO 77 again

--- backtracking/77Combinations.py
+++ backtracking/77Combinations.py
@@ -1,17 +1,25 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        def backtrack(first = 1, curr = []):
-            # if the combination is done
-            if len(curr) == k:  
-                output.append(curr[:])
-            for i in range(first, n + 1):
-                # add i into the current combination
-                curr.append(i)
-                # use next integers to complete the combination
-                backtrack(i + 1, curr)
-                # backtrack
-                curr.pop()
-        
+        '''
+        - Have a list to store 1d outputs
+        - Have an inner function to backtrack(curr_i, curr_list)
+            1. if len(curr_list) == k:
+                add to output as a copy
+                return
+            2. For(i = curr_i -> n+1):
+                    add to curr_list(i)
+                    backtrack(i + 1, curr_list)
+                    remove from curr_list(i)
+        '''
         output = []
-        backtrack()
+        def backtrack(curr_i, curr_list):
+            if len(curr_list) == k:
+                output.append(curr_list[:])
+                return
+            for i in range(curr_i, n + 1):
+                curr_list.append(i)
+                backtrack(i + 1, curr_list)
+                curr_list.pop()
+        backtrack(1, [])
         return output
+        
