1ec0ec1a7b75e288cefe427fedaff222f60f4323
Timothy Nguyen <quynhthoa1972@gmail.com>
2023-10-29 20:08:59 -0400

    New lc

--- binary-search/81SearchInRotatedSortedArray.py
+++ binary-search/81SearchInRotatedSortedArray.py
@@ -1,17 +1,22 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        if not nums or len(nums) == 0: return False
-        l = 0 
-        r = len(nums) - 1
-        while l <= r: 
+        l, r = 0, len(nums) - 1
+        while l <= r:
             m = (l + r) // 2
-            if nums[m] == target: return True
-            elif nums[m] > nums[l]:
-                if target >= nums[l] and target < nums[m]: r = m
-                else: l = m + 1
-            elif nums[m] < nums[l]: # so l..r is sorted
-                if target < nums[l] and target > nums[m]: l = m + 1
-                else: r = m 
-            else:
+            if nums[m] == target:
+                return True
+            if nums[l] == nums[m]:
                 l += 1
+                continue
+            if nums[l] <= nums[m]:
+                if nums[l] <= target <= nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                if nums[m] <= target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+                    
         return False
